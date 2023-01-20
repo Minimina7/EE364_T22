46f1b9c429458852812d33e65ac1ab6360a1a146
Moodz001 <123011894+Moodz001@users.noreply.github.com>
2023-01-20 19:14:41 +0300

    Add files via upload

---
+++ restaurant.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class restaurant {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+
+ public static int getRandom(int firstNumber, int secondNumber) {
+	 Random randomNumber = new Random();
+	 int random = randomNumber.nextInt(secondNumber)+ firstNumber;
+	 return random;}
+ }
