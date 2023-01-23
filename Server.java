25d2adc9c9d7f32be75e59c3f4150c91f52ae7c4
Moodz001 <123011894+Moodz001@users.noreply.github.com>
2023-01-23 19:55:45 +0300

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,4 +1,61 @@
+import java.util.ArrayList;
+
+// array of meal
+// array of snacks 
+//  order number 
+
+// data field
+//avalibale false
+//brost = order.brost
+
+/* check Snacks
+ *  
+ *  {
+
+
+/*
+/* check number of the order
+ * if n >1000 online
+ * else normal
+ * */
+
+/* check meal
+ * need avaliable variable 
+ * while loop bench emty
+ * remover order
+ * 
+ * need method take from bench void
+ * */
+
+/* check Snacks
+ * 
+ * */
+
+/* check sandwitch
+ * array sand 
+ * 
+ * */
+
+
+
+
 
 public class Server {
 
+	
+	public void preaperorder(int meal, ArrayList<Food> mealArray ) {
+		while (meal !=0) {
+			if (mealArray.size() >= 1) {
+				mealArray.remove(0);
+				meal--;}}
+		
+		
+	}
+	
 }
+
+
+
+
+
+
