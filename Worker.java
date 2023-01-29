2d3d252f6437e8c40b553c96864b0eeffd5480fa
Minimina7 <123008802+Minimina7@users.noreply.github.com>
2023-01-29 22:47:02 +0300

    added cashier class

--- Worker.java
+++ Worker.java
@@ -1,22 +1,24 @@
 
 public abstract class Worker {
-private int orderNumber;
-private Order customerOrder;
-private Customers customersAtservice;
-public int getOrderNumber() {
+private int orderNumber; // instance variable that save the order number
+private Order customerOrder; // instance variable that save the customer order
+private Customers customersAtservice; // instance variable that deal with the customers at service
+
+public int getOrderNumber() { // method to get the order number
 	return orderNumber;
 }
-public void setOrderNumber(int orderNumber) {
+public void setOrderNumber(int orderNumber) { // method to set order number
 	this.orderNumber = orderNumber;
 }
-public Order getCustomerOrder() {
+public Order getCustomerOrder() { // method to get customer order
 	return customerOrder;
 }
-public void setCustomerOrder(Order customerOrder) {
+public void setCustomerOrder(Order customerOrder) { // method to set customer order
 	this.customerOrder = customerOrder;
 }
-public abstract void serveCustomer(Customers customersAtservice);
-public Customers getServedCustomer() {
+public abstract Order serveCustomer(Customers customersAtservice); // abstract method to serve the customer
+
+public Customers getServedCustomer() { // method that return the service to the customer
 	return customersAtservice;
 }
 }
