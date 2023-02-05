488fa2df9d5f9c29323e3e3c3e00b31d280d3a2a
Minimina7 <123008802+Minimina7@users.noreply.github.com>
2023-02-05 19:42:24 +0300

    Create Cashier.java

---
+++ Cashier.java
@@ -0,0 +1,12 @@
+public class Cashier extends Worker{
+
+    public  void serveCustomer(Customers customersAtservice){
+        setCustomerOrder(customersAtservice);
+        setOrderNumber();
+        
+
+    }
+    
+
+
+}
