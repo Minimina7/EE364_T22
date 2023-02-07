dd679e7535946113301b4305ed5e2c0988c89d64
Moodz001 <123011894+Moodz001@users.noreply.github.com>
2023-02-07 16:58:20 +0300

    update

--- Restaurant.java
+++ Restaurant.java
@@ -1,23 +1,106 @@
-import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Random;
 public class Restaurant {
-	public static ArrayList<Drinks> Drink = new ArrayList<Drinks>();
-	public static ArrayList<Corn> Corn = new ArrayList<Corn>();
-	public static ArrayList<IceCream> IceCreams = new ArrayList<IceCream>();
-	public static ArrayList<JumboShrimp> JumboShrimp = new ArrayList<JumboShrimp>();
-	public static ArrayList<Nuggets> Nuggets = new ArrayList<Nuggets>();
-	public static ArrayList<Broast> Broast = new ArrayList<Broast>();
-	public static ArrayList<Sandwich> Sandwich = new ArrayList<Sandwich>();
+	
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
+		int normalServerNumber = 0 ; // check
+		int  onlineServerNumber = 1000; //check
+		
+		// customers array
+		ArrayList<Customers> Customersorder = new ArrayList<Customers>();
+
+		// bench creation
+		Bench<Broast> broast = new Bench<Broast>();
+		Bench<Broast> spicyBroast = new Bench<Broast>();
+		Bench<Nuggets> nuggets = new Bench<Nuggets>();
+		Bench<Nuggets> spicyNuggets = new Bench<Nuggets>();
+		Bench<JumboShrimp> jumboShrimp = new Bench<JumboShrimp>();
+		Bench<Sandwich> sandwich = new Bench<Sandwich>();
+		Bench<Corn> corn = new Bench<Corn>();
+		Bench<Drinks> drinks = new Bench<Drinks>();
+		Bench<IceCream> iceCream = new Bench<IceCream>();
+
+		// array of servers
+		ArrayList<Server> servers = new ArrayList<Server>();
+		
+		// Servers Creation
+		Server server1 = new Server(Customersorder, broast, spicyBroast, nuggets, spicyNuggets, jumboShrimp, sandwich,
+				corn, drinks, iceCream);
+		servers.add(server1);// add the server to the array
+		Server server2 = new Server(Customersorder, broast, spicyBroast, nuggets, spicyNuggets, jumboShrimp, sandwich,
+				corn, drinks, iceCream);
+		servers.add(server2);// add the server to the array
+		Server server3 = new Server(Customersorder, broast, spicyBroast, nuggets, spicyNuggets, jumboShrimp, sandwich,
+				corn, drinks, iceCream);
+		servers.add(server3);// add the server to the array
+		
+		// make the server work
+		for (Server server: servers ) {
+			
+					
+			server.takeNextOrder(onlineServerNumber, normalServerNumber);
+			if (server.isServerTookOnlineNumber()) {
+				onlineServerNumber+=1;
+			}
+			if (server.isServerTookNormalNumber()) {
+				normalServerNumber+=1;
+			}
+			if (server.isThereSpecialOrder()) {
+			}
+			
+			
+		}
+		
 		
 
 	}
 
 
  public static int getRandom(int firstNumber, int secondNumber) {
-	SecureRandom randomNumber = new SecureRandom();
+	 Random randomNumber = new Random();
 	 int random = randomNumber.nextInt(secondNumber)+ firstNumber;
 	 return random;}
+ 
+// public int getOnlineorder() {
+//	 return onlineorder;
+// }
+// 
+// 
+// public void setOnlineorder(int onlineorder) {
+//	 this.onlineorder = onlineorder;
+// }
+// 
+// 
+// public int getCashierorder() {
+//	 return cashierorder;
+// }
+// 
+// 
+// public void setCashierorder(int cashierorder) {
+//	 this.cashierorder = cashierorder;
+// }
+// 
+// 
+// public int getServerNumber() {
+//	 return serverNumber;
+// }
+// 
+// 
+// public void setServerNumber(int serverNumber) {
+//	 this.serverNumber = serverNumber;
+// }
+// 
+// 
+// public int getOnlineServerNumber() {
+//	 return onlineServerNumber;
+// }
+// 
+// 
+// public void setOnlineServerNumber(int onlineServerNumber) {
+//	 this.onlineServerNumber = onlineServerNumber;
+// }
  }
