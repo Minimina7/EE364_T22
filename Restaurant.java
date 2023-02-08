9d6cb4c25d011f4be91f43a0d5295c26a3c6ff23
Moodz001 <123011894+Moodz001@users.noreply.github.com>
2023-02-08 15:08:58 +0300

    up

--- Restaurant.java
+++ Restaurant.java
@@ -9,9 +9,12 @@ public class Restaurant {
 		
 		int normalServerNumber = 0 ; // check
 		int  onlineServerNumber = 1000; //check
+		double totalQualityAverage= 0;
+		int totalWaitingTime = 0;
 		
 		// customers array
-		ArrayList<Customers> Customersorder = new ArrayList<Customers>();
+		ArrayList<Customers> cashierCustomers = new ArrayList<Customers>();
+		ArrayList<Customers> serverCustomers = new ArrayList<Customers>();
 
 		// bench creation
 		Bench<Broast> broast = new Bench<Broast>();
@@ -23,18 +26,18 @@ public class Restaurant {
 		Bench<Corn> corn = new Bench<Corn>();
 		Bench<Drinks> drinks = new Bench<Drinks>();
 		Bench<IceCream> iceCream = new Bench<IceCream>();
-
+		
 		// array of servers
 		ArrayList<Server> servers = new ArrayList<Server>();
 		
 		// Servers Creation
-		Server server1 = new Server(Customersorder, broast, spicyBroast, nuggets, spicyNuggets, jumboShrimp, sandwich,
+		Server server1 = new Server(serverCustomers, broast, spicyBroast, nuggets, spicyNuggets, jumboShrimp, sandwich,
 				corn, drinks, iceCream);
 		servers.add(server1);// add the server to the array
-		Server server2 = new Server(Customersorder, broast, spicyBroast, nuggets, spicyNuggets, jumboShrimp, sandwich,
+		Server server2 = new Server(serverCustomers, broast, spicyBroast, nuggets, spicyNuggets, jumboShrimp, sandwich,
 				corn, drinks, iceCream);
 		servers.add(server2);// add the server to the array
-		Server server3 = new Server(Customersorder, broast, spicyBroast, nuggets, spicyNuggets, jumboShrimp, sandwich,
+		Server server3 = new Server(serverCustomers, broast, spicyBroast, nuggets, spicyNuggets, jumboShrimp, sandwich,
 				corn, drinks, iceCream);
 		servers.add(server3);// add the server to the array
 		
@@ -49,14 +52,65 @@ public class Restaurant {
 			if (server.isServerTookNormalNumber()) {
 				normalServerNumber+=1;
 			}
-			if (server.isThereSpecialOrder()) {
+			if (server.isThereSpecialOrder()) { // server need a sandwich not in the sandwiches bench
+				ArrayList<Sandwich> sandwiches = server.getSpecialSandwich();
+				// chief code  [ sadwiches]. // ask the chef to make the sandwich
 			}
 			
-			
+			if (server.isOrderIsReady()) { // the server prepared the order
+				
+				// bring customer information from the server
+				double foodQuality = server.getQuality();
+				ArrayList<Food> costumerOrder = server.getSubmittedOrder();
+				int costomerNumber = server.getCustomerOrderNumber();
+				int customerWaitingTime;
+
+				// deal with customer
+				for (int i = 0; i <= (serverCustomers.size() - 1); i++) {
+					Customers customer = serverCustomers.get(i);
+					if (customer.getOrderNumber() == costomerNumber) {
+						customer.setWaitingTime();
+						customer.setReceivedOrder(costumerOrder);
+						customer.setCustomerRating(foodQuality);
+						customerWaitingTime = customer.getWaitingTime();
+
+						// remove customer
+						serverCustomers.remove(i);
+
+						// gather information
+						totalQualityAverage += foodQuality;
+						totalWaitingTime += customerWaitingTime;
+
+						// brake the loop
+						break;
+					}
+
+				}
+			}
+
+		}
+		
+		// time effect
+
+		// customers
+		for (Customers customer : cashierCustomers) {
+			customer.setWaitingTime();
+		}
+
+		for (Customers customer : serverCustomers) {
+			customer.setWaitingTime();
+		}
+		
+		// Food
+		
+		for (Broast meal : broast.getBench()) {
+			meal.editQuality();
+		}
+		for (Broast meal : broast.getBench()) {
+			meal.editQuality();
 		}
 		
 		
-
 	}
 
 
@@ -64,6 +118,7 @@ public class Restaurant {
 	 Random randomNumber = new Random();
 	 int random = randomNumber.nextInt(secondNumber)+ firstNumber;
 	 return random;}
+ 
  
 // public int getOnlineorder() {
 //	 return onlineorder;
